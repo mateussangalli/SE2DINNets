7c8cdded5651ab11642c5bd45f4d4f7c1d7f70bd
mateussangalli <37818285+mateussangalli@users.noreply.github.com>
2022-02-21 10:21:50 +0100

    fixed dataset paths

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -6,8 +6,9 @@ out_dir = ''
 mnist12k_dir = 'mnist12k'
 mnist_rot_dir = 'mnist_rot'
 
-data_train_val = np.loadtxt('mnist/mnist_train.amat')
-data_test = np.loadtxt('mnist/mnist_test.amat')
+data_train_val = np.loadtxt(os.path.join(mnist12k_dir, 'mnist_train.amat'))
+data_test = np.loadtxt(os.path.join(mnist12k_dir, 'mnist_test.amat'))unzip mnist.zip
+unzip mnist_rotation_new.zip
 
 images_train_val = data_train_val[:, :-1].reshape(-1,28,28,1)
 labels_train_val = data_train_val[:, -1]
@@ -33,8 +34,8 @@ np.savez(os.path.join(out_dir, mnist12k_dir, 'test.npz'), data=images_test, labe
 ##################################################
 
 
-data_train_val = np.loadtxt('mnist_rotation_new/mnist_all_rotation_normalized_float_train_valid.amat')
-data_test = np.loadtxt('mnist_rotation_new/mnist_all_rotation_normalized_float_test.amat')
+data_train_val = np.loadtxt(os.path.join(mnist_rot_dir, 'mnist_all_rotation_normalized_float_train_valid.amat'))
+data_test = np.loadtxt(os.path.join(mnist_rot_dir, 'mnist_all_rotation_normalized_float_test.amat'))
 
 
 images_train_val = data_train_val[:, :-1].reshape(-1,28,28,1)
